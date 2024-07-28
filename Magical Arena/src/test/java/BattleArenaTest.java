c72c6d61c7001cabfc4c0931ac6ca6e311bbcd54
Aakash Natani <nataniaakash@gmail.com>
2024-07-28 18:53:31 +0530

    Implemented test for Player creation

--- Magical Arena/src/test/java/BattleArenaTest.java
+++ Magical Arena/src/test/java/BattleArenaTest.java
@@ -1,4 +1,15 @@
 package test.java;
 
+import main.java.Player;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class BattleArenaTest {
+    @Test
+    public void testPlayerCreation() {
+        Player player = new Player("Test Player", 50, 10, 5);
+        assertEquals(50, player.getHealth());
+        assertEquals(10, player.getStrength());
+        assertEquals(5, player.getAttack());
+    }
 }
